e177d1ceca2e334e1e87c13fbae96defaa82af4a
diego <diego@pop-os.localdomain>
2023-04-04 21:36:36 -0300

    separacion de test

---
+++ test_count_letters.py
@@ -0,0 +1,37 @@
+import unittest
+from count_letters import count_letters
+class TestCountLetters(unittest.TestCase):
+
+    def test_simple(self):
+        result = count_letters('a')
+        self.assertEqual(result, {'a': 1})
+
+    def test_complex(self):
+        result = count_letters('hola')
+        self.assertEqual(
+            result,
+            {
+                'h': 1,
+                'o': 1,
+                'l': 1,
+                'a': 1,
+            }
+        )
+
+    def test_super_complex(self):
+        result = count_letters('hola chau')
+        self.assertEqual(
+            result,
+            {
+                'h': 2,
+                'o': 1,
+                'l': 1,
+                'a': 2,
+                ' ': 1,
+                'c': 1,
+                'u': 1
+            }
+        )
+
+if __name__ == '__main__':
+    unittest.main()
