b779cf4fda608083b917a9e86432bad67365bda9
Diego3161 <d.xu@alumno.um.edu.ar>
2023-04-10 20:03:43 -0300

    nuevo test

--- test_count_words.py
+++ test_count_words.py
@@ -1,11 +1,29 @@
 import unittest
 from count_words import count_words
+
+
 class TestCountLetters(unittest.TestCase):
 
+    
     def test_simple(self):
-        result = count_words('hola')
-        self.assertEqual(result, {'hola': 1})
+        word = "hola"
+        result = count_words(word,texto)
+        self.assertEqual(result, {word:2})
 
+    def test_simple(self):
+        word = "amigo"
+        result = count_words(word,texto)
+        self.assertEqual(result, {word:1})
+    
+    def test_simple(self):
+        word = "como"
+        result = count_words(word,texto)
+        self.assertEqual(result, {word:1})
+
+    def test_simple(self):
+        word = "amiga"
+        result = count_words(word,texto)
+        self.assertEqual(result, {word:1})
 
 if __name__ == '__main__':
     unittest.main()
